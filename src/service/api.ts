3cd13cd4a75913c5a221a3bbe2305f7bf0fa9f1b
Amyyyy <dragonhel5@gmail.com>
2021-01-04 09:24:40 +0100

    allow credentials for login

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,7 +2,8 @@ import Axios, {AxiosInstance} from "axios";
 
 const apiUri: string = process.env.NODE_ENV === "production" ? "https://penweb.app/api" : "http://localhost:8181/api"
 const api: AxiosInstance = Axios.create({
-    baseURL: apiUri
+    baseURL: apiUri,
+    withCredentials: true
 })
 
 export {api, apiUri}
